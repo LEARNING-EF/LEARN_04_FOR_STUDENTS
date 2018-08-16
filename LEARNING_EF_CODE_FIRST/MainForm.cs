5a0f44cf10e367eaba3d55f8943c48964f976980
Dariush Tasdighi <dariush.tasdighi@hotmail.com>
2018-08-16 12:39:15 +0430

    Add project files.

---
+++ LEARNING_EF_CODE_FIRST/MainForm.cs
@@ -0,0 +1,14 @@
+﻿namespace LEARNING_EF_CODE_FIRST
+{
+	public partial class MainForm : System.Windows.Forms.Form
+	{
+		public MainForm()
+		{
+			InitializeComponent();
+		}
+
+		private void MainForm_Load(object sender, System.EventArgs e)
+		{
+		}
+	}
+}
---
+++ LEARNING_EF_CODE_FIRST/Models/DatabaseContext.cs
@@ -0,0 +1,15 @@
+﻿namespace Models
+{
+	public class DatabaseContext : System.Data.Entity.DbContext
+	{
+		static DatabaseContext()
+		{
+		}
+
+		public DatabaseContext() : base()
+		{
+		}
+
+		public System.Data.Entity.DbSet<Person> People { get; set; }
+	}
+}
